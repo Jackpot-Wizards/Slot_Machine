57e2588fa84f194403cb4ec65eb58ab703c582a6
Ignat Pechkurenko <pechkig@gmail.com>
2020-01-11 22:43:10 -0500

    Created empty project of Xcode 10.3 and IOS 12.4

---
+++ Slot_Machine/Slot_Machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slot_Machine
+//
+//  Created by Ignat Pechkurenko on 2020-01-11.
+//  Copyright © 2020 Jackpot-Wizards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
